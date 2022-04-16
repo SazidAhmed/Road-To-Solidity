9623b92682539a18ec8d52ea9340fcb624dec5e2
SazidAhmed <sazidahmed.official@gmail.com>
2022-04-16 14:08:22 +0600

    function visibility

--- Solidity/function.sol
+++ Solidity/function.sol
@@ -4,7 +4,27 @@ pragma solidity ^0.8.13;
 contract MyContract{
     uint a ;
     
-    function getValue() external view returns(uint){
+    //can be called only from inside
+    // function _getValue() private view returns(uint){
+    //     return a;
+    // }
+
+    //can be accessed by Inherit
+    // function _getValue() internal view returns(uint){
+    //     return a;
+    // }
+    
+    //can be called only from outside
+    // function getValue() external view returns(uint){
+    //     return a;
+    // }
+
+    //can be called from everywhere
+    function getValue() public view returns(uint){
         return a;
+    }
+
+    function setValue(uint _value) external{
+        a =_value;
     }
 }
