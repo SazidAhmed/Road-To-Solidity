599b19047ee8ce75813277a7013aaafc9f846354
SazidAhmed <sazidahmed.official@gmail.com>
2022-04-18 19:57:55 +0600

    Mapping crud

--- Solidity/mapping.sol
+++ Solidity/mapping.sol
@@ -2,9 +2,24 @@
 pragma solidity ^0.8.13;
 
 contract MyContract{
-    //declare mapping 
+    //declare mapping
     //crud
+    mapping(address => uint) balances;
+
+    function foo() external {
+        //add 
+        balances[msg.sender] = 100;
+        //read
+        balances[msg.sender];
+        //update
+        balances[msg.sender] = 200;
+        //delete
+        delete balances[msg.sender];
+
+    }
+    
     //default values
+    balances[someAddressThatDoNoExist] => 0;
     //nested mappings
     //array inside mapping
 }
